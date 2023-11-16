cf6f9d32a6885953bbdbd893777afc5add1319e2
sahilpabale <dev.sahilpabale@gmail.com>
2023-11-16 12:49:43 +0530

    feat: added pda seeder script

--- packages/helper-scripts/index.ts
+++ packages/helper-scripts/index.ts
@@ -1,11 +1,19 @@
 import { config } from 'dotenv';
 
-// import { generateSlug } from './src/data-move/generate_slug';
-import { getDataFromInfura, uploadToInfura } from './utils/uploadToInfura';
+import {
+  seedPDAInContributionV2,
+  // seedPDAInProject,
+  // seedPDAInProjectJoinRound,
+  // seedPDAInRound,
+  // seedPDAInUser,
+} from './utils/pdaSeeder';
 
-const apiKey = process.argv[2];
-const apiKeySecret = process.argv[3];
-const filePath = process.argv[4];
+// import { generateSlug } from './src/data-move/generate_slug';
+// import { getDataFromInfura, uploadToInfura } from './utils/uploadToInfura';
+
+// const apiKey = process.argv[2];
+// const apiKeySecret = process.argv[3];
+// const filePath = process.argv[4];
 
 console.log('Helper Script Started');
 
@@ -14,17 +22,18 @@ config();
 // generateSlug();
 async function run() {
   try {
-    const upResult = await uploadToInfura(apiKey, apiKeySecret, filePath);
-
-    const getResult = await getDataFromInfura(
-      apiKey,
-      apiKeySecret,
-      'QmVW4YdXa3FQ2z5yRvWJZj7U6y6m7Vz7xuQK5y7Z6RcYQF',
-      'output.json',
-    );
-
-    console.log('File uploaded successfully:', upResult);
-    console.log('File get successfully:', getResult.Data);
+    // const upResult = await uploadToInfura(apiKey, apiKeySecret, filePath);
+    // const getResult = await getDataFromInfura(
+    //   apiKey,
+    //   apiKeySecret,
+    //   'QmVW4YdXa3FQ2z5yRvWJZj7U6y6m7Vz7xuQK5y7Z6RcYQF',
+    //   'output.json',
+    // );
+    // await seedPDAInUser();
+    // await seedPDAInRound(); seed length error
+    // await seedPDAInProject();
+    // await seedPDAInProjectJoinRound(); // round data dont exist so couldn't seed
+    await seedPDAInContributionV2();
   } catch (error: any) {
     console.error('Error:', error.message);
   }

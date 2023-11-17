55a1f7d483cf1817421cda1eb87318dc23b0665a
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-11-17 21:47:23 +0530

    fix: remove duplicate emails package

--- packages/email/src/templates/generic.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-export const Generic: React.FC<any> = ({ projectName, contributorName }) => (
-  <div>
-    <h1>
-      {contributorName} contributed to {projectName}
-    </h1>
-  </div>
-);

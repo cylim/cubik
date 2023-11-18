adb4cf6bab9abbcea2529e91d7707a8cc3c4226d
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-11-18 11:05:47 +0530

    feat: added sponsor tabs

--- apps/cubik-dashboard/src/app/grants/components/sections/multisig-section/sponsor-multisig/SponsorMultisigCard.tsx
+++ apps/cubik-dashboard/src/app/grants/components/sections/multisig-section/sponsor-multisig/SponsorMultisigCard.tsx
@@ -2,7 +2,22 @@ import React from 'react';
 import { MultisigCard } from '@/app/grants/components/cards/MultisigCard';
 import { MultisigInfo } from '@/app/grants/components/sections/multisig-section/cubik-multisig/MultisigInfo';
 
-import { AvatarLabelGroup, Button, Icon, SubHead } from '@cubik/ui';
+import {
+  AvatarLabelGroup,
+  Button,
+  Icon,
+  SubHead,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+} from '@cubik/ui';
+
+import { Details } from './tabs/details';
+import { Matching } from './tabs/matching';
+import { Settings } from './tabs/settings';
+import { Transactions } from './tabs/transactions';
 
 export const SponsorMultisigCard = () => {
   return (
@@ -21,6 +36,30 @@ export const SponsorMultisigCard = () => {
           />
         }
       />
+      <Tabs size="sm" defaultValue={0}>
+        <TabList>
+          <div className="flex w-full gap-4 border-b border-[#333] bg-[#262626] pl-3">
+            <Tab value={0}>Details</Tab>
+            <Tab value={1}>Matching</Tab>
+            <Tab value={2}>Transactions</Tab>
+            <Tab value={3}>Settings</Tab>
+          </div>
+        </TabList>
+        <TabPanels>
+          <TabPanel value={0}>
+            <Details />
+          </TabPanel>
+          <TabPanel value={1}>
+            <Matching />
+          </TabPanel>
+          <TabPanel value={2}>
+            <Transactions />
+          </TabPanel>
+          <TabPanel value={3}>
+            <Settings />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
       <div className="flex h-[5rem] w-full items-center justify-center">
         <Button size={'md'} variant={'tertiary'}>
           <div className="flex items-center justify-center gap-2">
---
+++ apps/cubik-dashboard/src/app/grants/components/sections/multisig-section/sponsor-multisig/tabs/details.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { SubHead } from '@cubik/ui';
+
+export const Details = () => {
+  return (
+    <>
+      <div className="mx-auto flex w-full max-w-6xl flex-col items-start pt-5">
+        <SubHead size="xs" heading="Treasury Assets" />
+      </div>
+    </>
+  );
+};

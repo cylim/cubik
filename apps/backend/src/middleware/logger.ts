4c7bdf153d429ab57fd757d1ea203fd5803085d5
sahilpabale <dev.sahilpabale@gmail.com>
2023-11-30 21:26:05 +0530

    feat: add axiom for backend logs

--- apps/backend/src/middleware/logger.ts
+++ apps/backend/src/middleware/logger.ts
@@ -1,4 +1,6 @@
 import winston, { format } from 'winston';
+import { WinstonTransport as AxiomTransport } from '@axiomhq/winston';
+const {  errors } = winston.format;
 
 export const prettyJSON = (data: unknown) => JSON.stringify(data, null, 2);
 
@@ -7,6 +9,8 @@ const alignedWithColorsAndTime = format.combine(
   format.timestamp(),
   format.align(),
   format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`),
+  format.json(),
+  errors({ stack: true }),
 );
 
 const options = {
@@ -18,13 +22,24 @@ const options = {
   },
 };
 
+const axiomTransport = new AxiomTransport({
+  dataset: 'backend',
+  token: process.env.AXIOM_TOKEN as string,
+  orgId: process.env.AXIOM_ORG_ID as string,
+})
+
 // Don't enable slack in testing
-const transports: winston.transport[] = [];
+const transports: winston.transport[] = [
+  axiomTransport,
+];
 
 const logger = winston.createLogger({
+  levels: winston.config.npm.levels,
   level: 'info',
   format: alignedWithColorsAndTime,
   transports: [new winston.transports.Console(options.console), ...transports],
+  exceptionHandlers: [axiomTransport],
+  rejectionHandlers: [axiomTransport],
   exitOnError: false,
 });
 

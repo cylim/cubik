8289fc8fd6c01afb6c7b5d6f03a97c8a477d7ec4
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-12-06 12:16:29 +0530

    fix: build

--- apps/cubik-dashboard/src/components/Headers/index.tsx
+++ apps/cubik-dashboard/src/components/Headers/index.tsx
@@ -17,7 +17,12 @@ export const Header = () => {
           <div className="flex justify-start gap-10">
             <div className="flex items-center justify-center">
               <Logo />
-              <Tag size="desktop" color={'yellow'} variant="solid">
+              <Tag
+                border={true}
+                size="desktop"
+                color={'yellow'}
+                variant="solid"
+              >
                 Admin
               </Tag>
             </div>

390e4bf5bd4a3d530ecc1c5e1608e87e02581372
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-12-07 17:46:33 +0530

    fix: segment

--- packages/ui/components/ui/SegmentControl/Segment.tsx
+++ packages/ui/components/ui/SegmentControl/Segment.tsx
@@ -1,32 +1,61 @@
 import React from 'react';
 
+import { cn } from '../../../lib/utils';
 import { Text } from '../../ui/text/text';
 
 
 interface SegmentContainerProps {
   children: React.ReactNode;
+  size: 'l3' | 'l2' | 'l1';
 }
 interface SegmentItemsProps {
   children: React.ReactNode;
   onClick: () => void;
+<<<<<<< HEAD
 
 
 export const SegmentContainer = ({ children }: SegmentContainerProps) => {
+=======
+  isActive: boolean;
+}
+export const SegmentContainer = ({
+  children,
+  size = 'l2',
+}: SegmentContainerProps) => {
+>>>>>>> fca93e81 (fix: segment)
   return (
-    <div className="bg-red-400 w-max p-1 gap-1 flex justify-center items-center rounded-lg">
+    <div
+      className={cn(
+        size,
+        'bg-[var(--segment-control-surface-inactive)] w-full p-1 gap-1 flex justify-center items-center rounded-lg',
+      )}
+    >
       {children}
     </div>
   );
 };
 
+<<<<<<< HEAD
 
 export const SegmentItems = ({ children, onClick }: SegmentItemsProps) => {
+=======
+export const SegmentItems = ({
+  children,
+  onClick,
+  isActive,
+}: SegmentItemsProps) => {
+>>>>>>> fca93e81 (fix: segment)
   return (
     <div
       onClick={onClick}
-      className="bg-[var(--segment_control-surface-active)] p-1 rounded-lg"
+      className={cn(
+        'px-4 py-1 w-full cursor-pointer text-center rounded-lg shadow-[0px 1px 3px 0px rgba(0, 0, 0, 0.10), 0px 1px 2px -1px rgba(0, 0, 0, 0.10)]',
+        isActive
+          ? 'bg-[var(--segment-control-surface-active)]'
+          : 'bg-[var(--segment-control-surface-inactive)]',
+      )}
     >
-      <Text className="l1" color="primary">
+      <Text className="" color="primary">
         {children}
       </Text>
     </div>

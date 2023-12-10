189e3fdd9d980f5ff1019538e3a636d4a4d9d46d
ArpitaGanatra <ganatraarpita@gmail.com>
2023-12-10 19:15:36 +0530

    feat: tooltip comp

--- packages/ui/components/ui/Tooltip/Tooltip.tsx
+++ packages/ui/components/ui/Tooltip/Tooltip.tsx
@@ -1,27 +1,46 @@
 import React from 'react';
 import * as RadixTooltip from '@radix-ui/react-tooltip';
 
-interface Props {
-  children: React.ReactNode | React.JSX.Element;
-  trigger: React.ReactNode | React.JSX.Element;
+import { cn } from '../../../lib/utils';
+
+interface TooltipProps {
+  children: React.ReactNode;
 }
-export const Tooltip = ({ children, trigger }: Props) => {
+
+const Tooltip = ({ children }: TooltipProps) => {
   return (
-    <>
-      <RadixTooltip.Provider>
-        <RadixTooltip.Root>
-          <RadixTooltip.Trigger asChild>{trigger}</RadixTooltip.Trigger>
-          <RadixTooltip.Portal>
-            <RadixTooltip.Content
-              className="data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade text-violet11 select-none rounded-[4px] bg-white px-[15px] py-[10px] text-[15px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity]"
-              sideOffset={5}
-            >
-              {children}
-              <RadixTooltip.Arrow className="fill-white" />
-            </RadixTooltip.Content>
-          </RadixTooltip.Portal>
-        </RadixTooltip.Root>
-      </RadixTooltip.Provider>
-    </>
+    <RadixTooltip.Provider>
+      <RadixTooltip.Root>{children}</RadixTooltip.Root>
+    </RadixTooltip.Provider>
   );
 };
+
+interface TooltipTriggerProps {
+  children: React.ReactNode;
+}
+const TooltipTrigger = ({ children }: TooltipTriggerProps) => {
+  return <RadixTooltip.Trigger>{children}</RadixTooltip.Trigger>;
+};
+
+interface TooltipContentProps {
+  children: React.ReactNode;
+  className?: string;
+}
+const TooltipContent = ({ children, className }: TooltipContentProps) => {
+  return (
+    <RadixTooltip.Portal>
+      <RadixTooltip.Content
+        className={cn(
+          'data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade text-violet11 select-none rounded-lg bg-[var(--color-bg-info)] text-white px-[12px] py-[8px] text-[15px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity] max-w-xs',
+          className,
+        )}
+        sideOffset={5}
+      >
+        {children}
+        <RadixTooltip.Arrow className="fill-[var(--color-bg-info)]" />
+      </RadixTooltip.Content>
+    </RadixTooltip.Portal>
+  );
+};
+
+export { Tooltip, TooltipTrigger, TooltipContent };

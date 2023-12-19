b6860d4e82449c4bde1b75ea57febbf5d2606028
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-12-19 20:21:19 +0530

    fix: page route

--- apps/cubik-dashboard/src/app/grants/components/sections/details-section/segmentSwitch.tsx
+++ apps/cubik-dashboard/src/app/grants/components/sections/details-section/segmentSwitch.tsx
@@ -9,13 +9,25 @@ export const SegmentSwitch = () => {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const router = useRouter();
+  let path = pathname;
+  if (searchParams.get('project_page')) {
+    path = `${path}?project_page=${searchParams.get('project_page')}&`;
+  } else if (searchParams.get('contributors_page')) {
+    path = `${path}?contributors_page=${searchParams.get(
+      'contributors_page',
+    )}&`;
+  } else {
+    path = `${path}?`;
+  }
   return (
     <div className="flex flex-row gap-4">
       <SegmentContainer size="sm">
         <SegmentItems
-          isActive={searchParams.get('time') === '1W'}
+          isActive={
+            !searchParams.get('time') ? true : searchParams.get('time') === '1W'
+          }
           onClick={() => {
-            router.push(`${pathname}?time=1W`, {
+            router.push(`${path}time=1W`, {
               scroll: false,
             });
           }}
@@ -25,7 +37,7 @@ export const SegmentSwitch = () => {
         <SegmentItems
           isActive={searchParams.get('time') === '2W'}
           onClick={() => {
-            router.push(`${pathname}?time=2W`, { scroll: false });
+            router.push(`${path}time=2W`, { scroll: false });
           }}
         >
           2W
@@ -33,7 +45,7 @@ export const SegmentSwitch = () => {
         <SegmentItems
           isActive={searchParams.get('time') === '1M'}
           onClick={() => {
-            router.push(`${pathname}?time=1M`, { scroll: false });
+            router.push(`${path}time=1M`, { scroll: false });
           }}
         >
           1M
@@ -41,7 +53,7 @@ export const SegmentSwitch = () => {
         <SegmentItems
           isActive={searchParams.get('time') === '2M'}
           onClick={() => {
-            router.push(`${pathname}?time=2M`, { scroll: false });
+            router.push(`${path}time=2M`, { scroll: false });
           }}
         >
           2M

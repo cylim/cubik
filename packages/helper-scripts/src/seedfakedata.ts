776cc3e482dfb4bdaec48a02161b07838a980424
Sayan Biswas <me@sayanbiswas.in>
2023-12-21 21:09:53 +0530

    refactor: db seed
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>

--- packages/helper-scripts/src/seedfakedata.ts
+++ packages/helper-scripts/src/seedfakedata.ts
@@ -2,7 +2,13 @@ import fs from 'fs';
 import { faker } from '@faker-js/faker';
 import { Keypair } from '@solana/web3.js';
 
-import { prisma, User } from '@cubik/database';
+import { TokenList } from '@cubik/common';
+import {
+  prisma,
+  ProjectEventStatus,
+  ProjectVerifyStatus,
+  User,
+} from '@cubik/database';
 
 function generateFakeUser(): Partial<User> {
   const email = faker.internet.email();
@@ -17,13 +23,21 @@ function generateFakeUser(): Partial<User> {
     tx: faker.finance.ethereumAddress(),
     isActive: true,
     isArchive: false,
+    createdAt: faker.date.past(),
   };
 }
 
+/**
+ * Seeds fake users into the database.
+ * Generates and saves 20,000 fake user records using the `generateFakeUser` function.
+ * Each user record is created using the `prisma.user.create` method.
+ * If a user creation fails, an error message is logged to the console.
+ * Finally, the generated fake users are dumped as a JSON file named 'fakeUsers.json'.
+ */
 async function seedFakeUsers() {
   let generated = 0;
   const fakeUsers: Partial<User>[] = [];
-  while (generated < 3000) {
+  while (generated < 20_000) {
     const user = generateFakeUser();
     try {
       await prisma.user.create({
@@ -36,6 +50,7 @@ async function seedFakeUsers() {
           tx: user.tx!,
           isActive: true,
           isArchive: false,
+          createdAt: user.createdAt!,
         },
       });
       fakeUsers.push(user);
@@ -50,14 +65,25 @@ async function seedFakeUsers() {
   fs.writeFileSync('fakeUsers.json', json);
 }
 
+/**
+ * Seeds fake projects into the database.
+ * @returns {Promise<void>} A promise that resolves when the seeding is complete.
+ */
 async function seedFakeProjects() {
   let generated = 0;
-  const fakeUser = JSON.parse(
-    fs.readFileSync('fakeUsers.json').toString(),
-  ) as Partial<User>[];
-  while (generated <= 200) {
+  const users = await prisma.user.findMany({
+    select: {
+      id: true,
+      username: true,
+      mainWallet: true,
+    },
+  });
+  const status = Object.values(ProjectVerifyStatus);
+
+  while (generated <= 2000) {
     const name = faker.company.name();
-    const user = fakeUser[generated];
+    const user = users[generated];
+    const randomStatus = Math.floor(Math.random() * status.length);
     try {
       await prisma.project.create({
         data: {
@@ -68,6 +94,8 @@ async function seedFakeProjects() {
           email: faker.internet.email(),
           isArchive: false,
           industry: faker.company.buzzPhrase(),
+          status: status[randomStatus],
+          createdAt: faker.date.past(),
           owner: {
             connect: {
               id: user.id,
@@ -81,54 +109,6 @@ async function seedFakeProjects() {
       generated++;
     } catch (e) {
       console.log(`Failed to create project: ${name}`, e);
-    }
-  }
-}
-
-async function seedFakeContributions() {
-  const projects = await prisma.project.findMany({
-    select: {
-      id: true,
-      name: true,
-    },
-  });
-  const users = await prisma.user.findMany({
-    select: {
-      id: true,
-      mainWallet: true,
-      username: true,
-    },
-    take: 10,
-  });
-  for (const project of projects) {
-    for (const user of users) {
-      const usdc = faker.number.int();
-      try {
-        await prisma.contribution.create({
-          data: {
-            project: {
-              connect: {
-                id: project.id,
-              },
-            },
-            user: {
-              connect: {
-                id: user.id,
-              },
-            },
-            token: 'USDC',
-            totalAmount: usdc,
-            totalUsdAmount: usdc,
-            tx: user.mainWallet,
-            split: 0,
-          },
-        });
-        console.log(
-          `Created fake contribution on ${project.name} by ${user.username}`,
-        );
-      } catch (e) {
-        console.log(`Failed to create contribution: ${project.id}`);
-      }
     }
   }
 }
@@ -171,30 +151,105 @@ async function seedFakeJoinEvents() {
   const projects = await prisma.project.findMany({
     select: {
       id: true,
+      status: true,
     },
   });
   for (const event of events) {
     for (const project of projects) {
       try {
-        await prisma.projectJoinEvent.create({
-          data: {
-            event: {
-              connect: {
-                id: event.id,
+        const date = faker.date.past();
+        if (project.status === 'VERIFIED') {
+          const projectJoinEvent = await prisma.projectJoinEvent.create({
+            data: {
+              event: {
+                connect: {
+                  id: event.id,
+                },
               },
-            },
-            tx: faker.finance.ethereumAddress(),
-            type: 'ROUND',
-            project: {
-              connect: {
-                id: project.id,
+              tx: faker.finance.ethereumAddress(),
+              type: 'ROUND',
+              project: {
+                connect: {
+                  id: project.id,
+                },
               },
+              createdAt: date,
             },
-          },
-        });
-        console.log(`Created fake join event: ${event.id} and ${project.id}`);
+          });
+          await prisma.project.update({
+            data: {
+              projectJoinEvent: {
+                connect: {
+                  id: projectJoinEvent.id,
+                },
+              },
+              createdAt: date,
+            },
+            where: {
+              id: project.id,
+            },
+          });
+          console.log(`Created fake join event: ${event.id} and ${project.id}`);
+        }
       } catch (e) {
         console.log(`Failed to create join event: ${event.id}`);
+      }
+    }
+  }
+}
+
+async function seedFakeContributions() {
+  const projects = await prisma.project.findMany({
+    select: {
+      id: true,
+      name: true,
+      projectJoinEvent: true,
+    },
+  });
+  const users = await prisma.user.findMany({
+    select: {
+      id: true,
+      mainWallet: true,
+      username: true,
+    },
+    take: 1000,
+  });
+  for (const project of projects) {
+    for (const user of users) {
+      const token = TokenList[Math.floor(Math.random() * TokenList.length)];
+      const amt = Math.floor(Math.random() * 1000);
+      for (const projectJoinEvent of project.projectJoinEvent) {
+        if (
+          projectJoinEvent.projectEventStatus === ProjectEventStatus.APPROVED
+        ) {
+          try {
+            await prisma.contribution.create({
+              data: {
+                project: {
+                  connect: {
+                    id: project.id,
+                  },
+                },
+                user: {
+                  connect: {
+                    id: user.id,
+                  },
+                },
+                token: token.name,
+                totalAmount: amt,
+                totalUsdAmount: amt * 10,
+                tx: user.mainWallet,
+                split: 0,
+                createdAt: faker.date.past(),
+              },
+            });
+            console.log(
+              `Created fake contribution on ${project.name} by ${user.username}`,
+            );
+          } catch (e) {
+            console.log(`Failed to create contribution: ${project.id}`);
+          }
+        }
       }
     }
   }

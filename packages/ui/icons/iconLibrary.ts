ed76e3ac6294fed2b091a760296041e57a367d0b
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-12-25 14:47:25 +0530

    feat: wip

--- packages/ui/icons/iconLibrary.ts
+++ packages/ui/icons/iconLibrary.ts
@@ -7,7 +7,7 @@ Rules for adding new icons
 }
 
 export const iconLibrary = {
-  inbox: {
+  arrowRight: {
     viewBox: '0 0 24 24',
     paths: [
       'M15.17 6a30.23 30.23 0 0 1 5.62 5.406c.14.174.21.384.21.594m-5.83 6a30.232 30.232 0 0 0 5.62-5.406A.949.949 0 0 0 21 12m0 0H3',

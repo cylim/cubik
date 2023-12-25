4f9123ca9e77764c87964603bdc1126c2170e2a3
Sayan Biswas <me@sayanbiswas.in>
2023-12-25 20:51:41 +0530

    refactor: useTabMeasurements to improve performance and readability
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>

--- packages/ui/components/ui/Tabs/useTabMeasurements.tsx
+++ packages/ui/components/ui/Tabs/useTabMeasurements.tsx
@@ -1,4 +1,4 @@
-import { useLayoutEffect, useState } from 'react';
+import { useCallback, useLayoutEffect, useRef, useState } from 'react';
 
 type Measurements = {
   left: number;
@@ -14,30 +14,38 @@ export function useTabMeasurements(
     tabRefs.map(() => ({ left: 0, width: 0 })),
   );
 
+  const updateMeasurements = useCallback(() => {
+    const containerRect = tabListContainerRef?.getBoundingClientRect();
+
+    const newMeasurements = tabRefs.map((tabRef) => {
+      if (tabRef && tabRef.offsetParent !== null) {
+        const tabRect = tabRef.getBoundingClientRect();
+        const left = tabRect.left - (containerRect?.left ?? 0);
+        const { width } = tabRect;
+        return { left, width };
+      }
+      return { left: 0, width: 0 };
+    });
+
+    setMeasurements(newMeasurements);
+  }, [tabRefs, tabListContainerRef]);
+
+  const observerRef = useRef<ResizeObserver>();
+
+  if (!observerRef.current) {
+    observerRef.current = new ResizeObserver(updateMeasurements);
+  }
+
+  const { current: observer } = observerRef;
+
   useLayoutEffect(() => {
     if (!tabListContainerRef) {
       return;
     }
-
-    const updateMeasurements = () => {
-      const containerRect = tabListContainerRef.getBoundingClientRect();
-
-      const newMeasurements = tabRefs.map((tabRef) => {
-        if (tabRef && tabRef.offsetParent !== null) {
-          const tabRect = tabRef.getBoundingClientRect();
-          const left = tabRect.left - containerRect.left;
-          const { width } = tabRect;
-          return { left, width };
-        }
-        return { left: 0, width: 0 };
-      });
-
-      setMeasurements(newMeasurements);
-    };
-
-    const observer = new ResizeObserver(updateMeasurements);
+    observerRef.current = new ResizeObserver(updateMeasurements);
     tabRefs.forEach((ref) => {
       if (ref) {
+        observer.unobserve(ref);
         observer.observe(ref);
       }
     });
@@ -45,7 +53,7 @@ export function useTabMeasurements(
     return () => {
       observer.disconnect();
     };
-  }, [tabRefs, tabListContainerRef]);
+  }, [tabRefs, tabListContainerRef, updateMeasurements, observer]);
 
   return measurements;
 }

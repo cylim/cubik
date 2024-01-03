026bd8172cff397e9a47569455553eead8824827
Sayan Biswas <me@sayanbiswas.in>
2024-01-03 14:00:30 +0530

    chore: resolve issues in project actions.ts
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>

--- apps/web/src/app/project/[slug]/actions.ts
+++ apps/web/src/app/project/[slug]/actions.ts
@@ -1,34 +1,46 @@
 'use server'
 
+import { IsUserLoginServer } from "@/utils/auth/isUserLoginServer";
+import { cookies } from "next/headers";
+
 /**
  * Creates a comment for a project.
- * @param comment - The content of the comment.
- * @param userId - The ID of the user creating the comment.
- * @param projectId - The ID of the project the comment belongs to.
+ * 
+ * @param comment - The comment text.
+ * @param projectId - The ID of the project.
  * @returns The created comment.
- * @throws Error if the user is not logged in.
+ * @throws Error if the user is not logged in or if the comment is empty.
+ * @throws Error if there is an error creating the comment.
  */
-const makeComment = async (comment: string, userId: string, projectId: string) => {
-    if (!userId) {
+const makeComment = async (comment: string, projectId: string) => {
+    const cookieStore = cookies();
+    const token = cookieStore.get('authToken');
+    const user = token && (await IsUserLoginServer(token.value));
+    if (!user) {
         throw new Error('User not logged in');
     }
     if (!comment) {
-        return;
+        throw new Error('Comment cannot be empty');
     }
-    const cmt = await prisma.comments.create({
-        data: {
-            userId: userId,
-            comment: comment,
-            // @ts-ignore
-            projectId: projectId,
-            reactions: {}
-        },
-        select: {
-            project: true
-        }
-    });
-    console.log('cmt - ', cmt);
-    return cmt;
+    try {
+        const cmt = await prisma.comments.create({
+            data: {
+                userId: user.id,
+                comment: comment,
+                projectId: projectId,
+                reactions: {}
+            },
+            select: {
+                project: true
+            }
+        });
+        console.log('cmt - ', cmt);
+        return cmt;
+    }
+    catch (e) {
+        console.log('error - ', e);
+        throw new Error('Failed to create comment');
+    }
 }
 
 /**
@@ -74,7 +86,7 @@ const getComments = async ({
     });
 }
 
-const unused_get_contribs = async ({
+const unusedGetContribs = async ({
     slug,
     isArchive = false,
     page = 1,
@@ -120,15 +132,15 @@ const unused_get_contribs = async ({
 /**
  * Retrieves contributions for a specific project and event.
  * 
- * @param project_slug - The slug of the project.
- * @param event_id - The ID of the event.
+ * @param projectSlug - The slug of the project.
+ * @param eventId - The ID of the event.
  * @param page - The page number of the results.
  * @param limit - The maximum number of contributions per page.
  * @returns An object containing the contributions data, total count, and total number of pages.
  */
 const getContributions = async (
-    project_slug: string,
-    event_id: string,
+    projectSlug: string,
+    eventId: string,
     page: number,
     limit: number
 ) => {
@@ -138,9 +150,9 @@ const getContributions = async (
         prisma.contribution.findMany({
             where: {
                 project: {
-                    slug: project_slug
+                    slug: projectSlug
                 },
-                eventId: event_id
+                eventId: eventId
             },
             skip: skip,
             take: limit,
@@ -166,9 +178,9 @@ const getContributions = async (
         prisma.contribution.count({
             where: {
                 project: {
-                    slug: project_slug
+                    slug: projectSlug
                 },
-                eventId: event_id
+                eventId: eventId
             }
         })
     ])
@@ -181,17 +193,17 @@ const getContributions = async (
 /**
  * Retrieves the top earners for a given project and event.
  * 
- * @param project_slug - The slug of the project.
- * @param event_id - The ID of the event.
+ * @param projectSlug - The slug of the project.
+ * @param eventId - The ID of the event.
  * @returns An array of top earners, sorted by the total USD amount earned.
  */
-const getTopEarner = async (project_slug: string, event_id: string) => {
+const getTopEarner = async (projectSlug: string, eventId: string) => {
     const res = await prisma.contribution.findMany({
         where: {
             project: {
-                slug: project_slug
+                slug: projectSlug
             },
-            eventId: event_id
+            eventId: eventId
         },
         select: {
             userId: true,
@@ -235,11 +247,11 @@ const getTopEarner = async (project_slug: string, event_id: string) => {
     });
 };
 
-const getTeam = async (project_slug: string) => {
+const getTeam = async (projectSlug: string) => {
     return await prisma.team.findMany({
         where: {
             project: {
-                slug: project_slug
+                slug: projectSlug
             }
         },
         select: {

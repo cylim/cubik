8b6d62c18b9f5e9635c0fd9dadb99a4eb38f50dd
Sayan Biswas <me@sayanbiswas.in>
2024-01-06 20:07:10 +0530

    Dashboard project datafetch (#476)
    * feat: [dashboard] initial project fetch api
    
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>
    
    * feat: [dashboard] initial project page
    
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>
    
    * feat: add useProjects hook for querying projects
    
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>
    
    * feat: [dashboard/project/api] allow searching by industry
    
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>
    
    * feat: [dashboard/useProjects] update hook to support optional params with proper url building
    
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>
    
    * feat: add pagination and routing functionality to project page
    
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>
    
    * feat: [dashboard/project/api] allow searching by project name and send accurate result & pages count
    
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>
    
    * feat: [dashbord] add search to project page
    
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>
    
    * feat: [dashboard/useProjects] update hook to support project search
    
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>
    
    * chore: [schema.prisma] add fulltext search support for project names
    
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>
    
    * feat: [dashboard/project/api & useProjects] fetch createdAt, link, email & owner of project
    
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>
    
    * feat: [dashboard/project] use table to display projects
    
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>
    
    * fix: [web/projects] update slides data type
    
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>
    
    * feat: [dashboard/project/api] select team, slides, links and description
    
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>
    
    * feat: create drawer component and allow accept or decline feature
    TODO: make it slide sideways
    
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>
    
    * feat: [dashboard/useProjects] update hook to handle new attributes
    
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>
    
    * feat: new update project state server action
    
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>
    
    * feat: [dashboard/project] add projectdrawer
    
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>
    
    * feat: add sheet component
    
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>
    
    * feat: [dashboard/project] use sheet component for projectdrawer, now it opens from right side
    
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>
    
    * fix: [dashboard] add skeleton to project page
    
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>
    
    * fix: [dashboard] style tag component and fix width of segment, also add to icon search box
    
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>
    
    * feat: [dashboard/project/api] dont show projects in draft state
    
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>
    
    ---------
    
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>

---
+++ apps/cubik-dashboard/src/hooks/projects/useProjects.ts
@@ -0,0 +1,96 @@
+import { ProjectVerifyStatus } from "@cubik/database";
+import { ApiResponseType, apiInstance } from "@cubik/database/api";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { toast } from "sonner";
+
+type Params = {
+    page: number;
+    limit: number;
+    projectStatus?: ProjectVerifyStatus;
+    searchIndustry?: string;
+    search?: string;
+}
+
+export interface Result {
+    projects: Project[];
+    numResults: number;
+    totalPages: number;
+}
+
+export interface Project {
+    id: string;
+    name: string;
+    shortDescription: string;
+    logo: string;
+    industry: string[];
+    slug: string;
+    createdAt: Date;
+    status: ProjectVerifyStatus;
+    projectLink: string;
+    twitterHandle: string;
+    githubLink: string;
+    email: string;
+    slides: string[];
+    longDescription: string;
+    owner: {
+        id: string;
+        username: string;
+        profilePicture: string;
+        mainWallet: string;
+    };
+    team: {
+        user: {
+            id: string;
+            username: string;
+            mainWallet: string;
+            profilePicture: string;
+        };
+    }[];
+}
+
+type ResponseType = ApiResponseType & {
+    result: Result;
+};
+
+const queryFn = async ({ page, limit, projectStatus, searchIndustry, search }: Params) => {
+    const requestUrl = new URL('/api/projects', window.origin);
+    requestUrl.searchParams.set('page', page.toString());
+    requestUrl.searchParams.set('limit', limit.toString());
+    if (projectStatus) {
+        requestUrl.searchParams.set('projectStatus', projectStatus);
+    }
+    if (searchIndustry) {
+        requestUrl.searchParams.set('industry', searchIndustry);
+    }
+    if (search) {
+        requestUrl.searchParams.set('search', search);
+    }
+    const response = await apiInstance.get(requestUrl.toString());
+    const responseData = response.data as ResponseType;
+    if (!responseData.success) {
+        toast.error(responseData.message);
+        throw new Error(responseData.message);
+    }
+    return responseData.result as Result;
+}
+
+/**
+ * Custom hook for fetching projects.
+ *
+ * @param {Params} params - page number, limit, project status, and search industry.
+ * @returns {QueryResult} - The result of the query.
+ */
+const useProjects = ({ page, limit, projectStatus, searchIndustry, search }: Params) => {
+    return useInfiniteQuery({
+        queryKey: ['projects', { page, limit, projectStatus, searchIndustry, search }],
+        queryFn: () => queryFn({ page, limit, projectStatus, searchIndustry, search }),
+        getNextPageParam: (lastPage, pages) => {
+            if (lastPage.totalPages === pages.length) {
+                return undefined;
+            }
+            return pages.length + 1;
+        }
+    })
+}
+
+export default useProjects;

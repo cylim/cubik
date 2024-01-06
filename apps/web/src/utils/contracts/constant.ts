ec92707f0f2d15db87deac0165da626160222e60
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2024-01-06 20:08:23 +0530

    feat: contract changes

---
+++ apps/web/src/utils/contracts/constant.ts
@@ -0,0 +1,5 @@
+import { web3 } from '@coral-xyz/anchor';
+
+export const PROGRAM_ID = new web3.PublicKey(
+  '3s9zZaosL6hJFeDToXDoPN4sQgyVwLEdqzaztZXj1Nnk',
+);
--- apps/web/src/utils/contracts/project.ts
+++ apps/web/src/utils/contracts/project.ts
@@ -1,5 +1,16 @@
+import { PROGRAM_ID } from '@/utils/contracts/constant';
 import * as anchor from '@coral-xyz/anchor';
 
-import '@cubik-so/sdk/src';
-
-export const getProjectPDA = () => {};
+export const getProjectPDA = (
+  create_key: anchor.web3.PublicKey,
+  counter: number,
+): anchor.web3.PublicKey => {
+  return anchor.web3.PublicKey.createProgramAddressSync(
+    [
+      anchor.utils.bytes.utf8.encode('project'),
+      create_key.toBuffer(),
+      new anchor.BN(counter).toArrayLike(Buffer, 'le', 2),
+    ],
+    PROGRAM_ID,
+  );
+};

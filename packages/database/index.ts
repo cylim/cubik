f6815b955bed5f92d2e5bf4f1345b2805c98cf7a
sahilpabale <dev.sahilpabale@gmail.com>
2024-01-10 15:44:28 +0530

    fix: db migration and image server route

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -2,25 +2,33 @@
 import { PrismaClient } from '@prisma/client';
 import { config } from 'dotenv';
 
+import { PrismaClient as OldPrismaClient } from './prisma/client/old';
+
 export * from '@prisma/client';
+// export * from './prisma/client/old';
 
 config();
 declare global {
   var prisma: PrismaClient;
+  var oldPrisma: OldPrismaClient;
 }
 
 let prisma: PrismaClient;
+let oldPrisma: OldPrismaClient;
 
 if (typeof window === 'undefined') {
   if (process.env.NODE_ENV === 'production') {
     prisma = new PrismaClient();
+    oldPrisma = new OldPrismaClient();
   } else {
     if (!global.prisma) {
       global.prisma = new PrismaClient();
+      global.oldPrisma = new OldPrismaClient();
     }
     prisma = global.prisma;
+    oldPrisma = global.oldPrisma;
   }
 }
 
 // const prisma = new PrismaClient();
-export { prisma };
+export { prisma, oldPrisma };
--- packages/helper-scripts/cli.ts
+++ packages/helper-scripts/cli.ts
@@ -1,15 +1,22 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import {
-  createComponentNames,
-  generateSemantic,
-} from './src/color/generateCSS';
-import { convertToCSSVariables } from './src/color/primitive';
-import { moveDataDevDB } from './src/data-move/move-dev-db';
-import { replaceSvgWithBackup } from './src/svgDetect';
+// import { prisma } from '@cubik/database';
+
+// import { uploadURLsToCloudflare } from '../helper-scripts/utils/uploadToCloudflare';
+// import {
+//   createComponentNames,
+//   generateSemantic,
+// } from './src/color/generateCSS';
+// import { convertToCSSVariables } from './src/color/primitive';
+import { moveTeamData } from './src/migration/moveTeam';
+
+// import { moveDataDevDB } from './src/data-move/move-dev-db';
+// import { replaceSvgWithBackup } from './src/svgDetect';
 
 // generateSemantic();
 // createComponentNames();
 // replaceSvgWithBackup();
 // convertToCSSVariables();
 
-moveDataDevDB();
+moveTeamData().catch((e) => {
+  console.error(e);
+});

7a0ca418f8f39fa2a0d25deb8753ba2d73a282e5
dhruvsol <89624156+dhruvsol@users.noreply.github.com>
2024-01-12 12:42:10 +0530

    fix: connections params (#496)

--- apps/web/src/utils/contracts/constant.ts
+++ apps/web/src/utils/contracts/constant.ts
@@ -1,9 +1,8 @@
 import { env } from '@/env.mjs';
 import { web3 } from '@coral-xyz/anchor';
 
-export const PROGRAM_ID = new web3.PublicKey(
-  '3s9zZaosL6hJFeDToXDoPN4sQgyVwLEdqzaztZXj1Nnk',
-);
+import { getConnection } from '@cubik/common/solana';
+
 export const SQUADS_PROGRAM_ID = new web3.PublicKey(
   'SQDS4ep65T869zMMBKyuUq6aD6EgTu8psMjkvj52pCf',
 );
@@ -14,7 +13,4 @@ function toUtfBytes(str: string): Uint8Array {
 export const SEED_PREFIX = toUtfBytes('multisig');
 export const SEED_MULTISIG = toUtfBytes('multisig');
 
-export const connection = new web3.Connection(
-  env.NEXT_PUBLIC_RPC_DEVNET_URL,
-  'confirmed',
-);
+export const connection = getConnection('confirmed');

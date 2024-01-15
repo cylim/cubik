4344db24d90118f0edea68dd8f512a700d9ad806
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2024-01-15 23:30:50 +0530

    feat: wip

--- apps/web/src/app/p/[slug]/layout.tsx
+++ apps/web/src/app/p/[slug]/layout.tsx
@@ -6,100 +6,12 @@ import { utils } from '@coral-xyz/anchor';
 
 import { Prisma, prisma } from '@cubik/database';
 
-interface OgProps {
-  params: { slug: string };
-  searchParams: Record<string, string | string[] | undefined>;
-}
-
-export async function generateMetadata(
-  { params }: OgProps,
-  parent?: ResolvingMetadata,
-): Promise<Metadata> {
-  let contributors = 0;
-  let eventName = undefined;
-  const project = await prisma.project.findFirst({
-    where: {
-      slug: params.slug,
-    },
-    select: {
-      name: true,
-      shortDescription: true,
-      logo: true,
-      _count: {
-        select: {
-          comments: true,
-        },
-      },
-    },
-  });
-
-  const newImage = `/api/og?name=${utils.bytes.base64.encode(
-    Buffer.from(project?.name ?? 'default'),
-  )}&tagline=${utils.bytes.base64.encode(
-    Buffer.from(project?.shortDescription ?? 'default'),
-  )}&logo=${utils.bytes.base64.encode(
-    Buffer.from(project?.logo ?? 'default'),
-  )}&contributors=${contributors}&comments=${
-    project?._count.comments
-  }&eventName=${eventName}`;
-
-  const previousImages = (await parent)?.openGraph?.images ?? [];
-
-  return {
-    title: project?.name,
-    description: project?.shortDescription,
-    metadataBase: new URL('https://www.cubik.so'),
-    openGraph: {
-      type: 'website',
-      images: [`${newImage}`, ...previousImages],
-      title: project?.name,
-      description: project?.shortDescription,
-    },
-    twitter: {
-      card: 'summary_large_image',
-      images: [`${newImage}`, ...previousImages],
-      title: project?.name,
-      description: project?.shortDescription,
-    },
-  };
-}
-
 interface Props {
   params: {
     slug: string;
   };
   children: React.JSX.Element | React.JSX.Element[];
 }
-
-export type ProjectHeadersType = Prisma.ProjectGetPayload<{
-  select: {
-    id: true;
-    name: true;
-    shortDescription: true;
-    logo: true;
-    projectLink: true;
-    projectJoinEvent: {
-      select: {
-        id: true;
-        event: {
-          select: {
-            id: true;
-            name: true;
-            type: true;
-            isActive: true;
-            eventStatus: {
-              select: {
-                status: true;
-                endTime: true;
-                startTime: true;
-              };
-            };
-          };
-        };
-      };
-    };
-  };
-}>;
 
 const fetchProject = async (slug: string) => {
   try {

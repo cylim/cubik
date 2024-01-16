aed751c30250a5fb52e579475500efd09df6c5b9
sahilpabale <dev.sahilpabale@gmail.com>
2024-01-16 20:50:42 +0530

    feat: image cache location & revalidation

--- apps/backend/src/controllers/images.controller.ts
+++ apps/backend/src/controllers/images.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import fs from 'fs';
 import path from 'path';
 import { Readable } from 'stream';
@@ -9,9 +10,40 @@ import validator from 'validator';
 
 const cache = new NodeCache({ stdTTL: 100, checkperiod: 120 });
 
-export const imageFilter = async (req: Request, res: Response) => {
-  // @todo -- revalidate cache every 24 hrs
+// Add this function to revalidate the cache
+export const revalidateCache = async () => {
+  const keys = cache.keys();
+  for (const key of keys) {
+    try {
+      // Extract size and URL from the key
+      const [size, url] = key.split('-');
+      const [width, height] = size.split('x').map((s) => parseInt(s, 10));
 
+      // Fetch the image again
+      const response = await axios.get(url, { responseType: 'arraybuffer' });
+      const buffer = Buffer.from(response.data, 'binary');
+
+      // Resize and reformat the image
+      const resizedImage = await sharp(buffer)
+        .resize(width, height)
+        .toFormat('png')
+        .toBuffer();
+
+      // Update the cache with the new image
+      const cacheDir =
+        process.env.NODE_ENV !== 'production'
+          ? path.join(__dirname, '../../../../cache')
+          : '/root/cache/images';
+      const resizedImagePath = path.join(cacheDir, encodeURIComponent(key));
+      fs.writeFileSync(resizedImagePath, resizedImage, { flag: 'w' });
+      cache.set(key, resizedImagePath);
+    } catch (error) {
+      console.error(`Error revalidating cache for key ${key}:`, error);
+    }
+  }
+};
+
+export const imageFilter = async (req: Request, res: Response) => {
   // /:size/:url --> 500x500/<url>
   const { size, url } = req.query as {
     size: string;
@@ -23,9 +55,6 @@ export const imageFilter = async (req: Request, res: Response) => {
       error: 'Missing required fields',
     });
   }
-
-  console.log('size', size);
-  console.log('url', url);
 
   const [width, height] = size.split('x').map((s) => parseInt(s, 10));
 
@@ -56,7 +85,13 @@ export const imageFilter = async (req: Request, res: Response) => {
     });
     const buffer = Buffer.from(response.data, 'binary');
 
-    const cacheDir = path.join(__dirname, '../../../../cache');
+    let cacheDir;
+    if (process.env.NODE_ENV !== 'production') {
+      cacheDir = path.join(__dirname, '../../../../cache');
+    } else {
+      cacheDir = '/root/cache/images';
+    }
+
     if (!fs.existsSync(cacheDir)) {
       fs.mkdirSync(cacheDir, { recursive: true });
     }

7b55dc1ae99b090654f1eb26a17464e3d111a7ea
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2024-01-16 09:58:17 +0530

    fix: logger

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,30 +1,31 @@
-import { ILogObj, Logger as TsLogger } from 'tslog';
+// import { ILogObj, Logger as TsLogger } from 'tslog';
 
 export * from './axiom';
 
-const tslog: TsLogger<ILogObj> = new TsLogger({
-  prettyLogTimeZone: 'UTC',
-  prettyErrorStackTemplate:
-    '  • {{fileName}}\t{{method}}\n\t{{filePathWithLine}}', // default
-  prettyErrorTemplate:
-    '\n{{errorName}} {{errorMessage}}\nerror stack:\n{{errorStack}}', // default
-  //   prettyLogTemplate: '{{hh}}:{{MM}}:{{ss}}:{{ms}}\t{{logLevelName}}', // default with exclusion of `{{filePathWithLine}}`
-  stylePrettyLogs: true,
-  prettyLogStyles: {
-    name: 'yellow',
-    dateIsoStr: 'blue',
-  },
-  type: 'pretty',
-});
+// const tslog: TsLogger<ILogObj> = new TsLogger({
+//   prettyLogTimeZone: 'UTC',
+//   prettyErrorStackTemplate:
+//     '  • {{fileName}}\t{{method}}\n\t{{filePathWithLine}}', // default
+//   prettyErrorTemplate:
+//     '\n{{errorName}} {{errorMessage}}\nerror stack:\n{{errorStack}}', // default
+//   //   prettyLogTemplate: '{{hh}}:{{MM}}:{{ss}}:{{ms}}\t{{logLevelName}}', // default with exclusion of `{{filePathWithLine}}`
+//   stylePrettyLogs: true,
+//   prettyLogStyles: {
+//     name: 'yellow',
+//     dateIsoStr: 'blue',
+//   },
+//   type: 'pretty',
+// });
 export const logInfo = (data: any) => {
   //   tslog.info(data);
-  const a = tslog.getSubLogger({
-    name: 'info',
-    type: 'pretty',
-    prefix: ['payment-service:stripe'],
-  });
-  a.info(data);
+  // const a = tslog.getSubLogger({
+  //   name: 'info',
+  //   type: 'pretty',
+  //   prefix: ['payment-service:stripe'],
+  // });
+  // a.info(data);
+  console.log(data);
 };
 export const logE = (data: any) => {
-  tslog.error(data);
+  console.error('Error :-', data);
 };

be783f46116ad71a421562ded90c453e7d751b02
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2024-01-16 19:57:27 +0530

    fix: sns domain

--- packages/common/solana/sns.ts
+++ packages/common/solana/sns.ts
@@ -1,26 +1,28 @@
-'use client';
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { web3 } from '@coral-xyz/anchor';
+// @ts-ignore
+import { walletAddressToDotSol } from '@portal-payments/solana-wallet-names';
 
-// import { getDomainsResult } from '@bonfida/sns-react';
-// import { web3 } from '@coral-xyz/anchor';
+import { getConnection } from './connection';
 
 /**
  * @name getSNSFromAddress
  * @description
  * get sns from address
  * @param address: solana address
- * @param  rpc: rpc url
  * @return sns: string
  * @example
  * const sns = await getSNSFromAddress('address');
  * @example
  * const sns = await getSNSFromAddress('address', 'https://api.mainnet-beta.solana.com');
  */
-
-// export const getSNSFromAddress = async (address: string, rpc?: string) => {
-//   // const connection = new web3.Connection(
-//   //   rpc || 'https://api.mainnet-beta.solana.com',
-//   // );
-
-//   const domain = await getDomainsResult(connection, [address]);
-//   return domain[0];
-// };
+export const getSNSFromAddress = async (
+  address: string,
+): Promise<string | null> => {
+  const domain = await walletAddressToDotSol(
+    getConnection('confirmed', 'mainnet-beta'),
+    new web3.PublicKey(address),
+  );
+  console.log('domain', domain.walletName);
+  return domain.walletName || null;
+};

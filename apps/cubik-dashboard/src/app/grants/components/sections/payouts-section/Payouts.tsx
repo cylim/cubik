698c64e7b0d1b4e4e1708585c857505fbc0bcb9b
ArpitaGanatra <ganatraarpita@gmail.com>
2024-01-21 13:09:26 +0530

    feat: payouts table intiate

--- apps/cubik-dashboard/src/app/grants/components/sections/payouts-section/Payouts.tsx
+++ apps/cubik-dashboard/src/app/grants/components/sections/payouts-section/Payouts.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { PartnerTreasuries } from '@/app/grants/components/sections/payouts-section/PartnerTreasuries';
+import ProjectTransactions from '@/app/grants/components/sections/payouts-section/ProjectTransactions';
 
 const PayoutsSection = () => {
   return (
     <div>
+      <ProjectTransactions />
       <PartnerTreasuries />
     </div>
   );
---
+++ apps/cubik-dashboard/src/app/grants/components/sections/payouts-section/ProjectTransactions.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { SubHead } from '@cubik/ui';
+
+const ProjectTransactions = () => {
+  return (
+    <div>
+      <div className="py-11">
+        <SubHead heading="Payouts" />
+      </div>
+    </div>
+  );
+};
+
+export default ProjectTransactions;

e705c22c1a373f155f0c974a8fdef1f9f0bf8ae2
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2024-01-21 14:29:23 +0530

    fix: prisma global issue

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -5,21 +5,11 @@ import { config } from 'dotenv';
 export * from '@prisma/client';
 
 config();
-// declare global {
-// var prisma: PrismaClient;
-// }
 
 let prisma: PrismaClient;
 
 if (typeof window === 'undefined') {
-  if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient();
-  } else {
-    // if (!global.prisma) {
-    //   global.prisma = new PrismaClient();
-    // }
-    // prisma = global.prisma;
-  }
+  prisma = new PrismaClient();
 }
 
 export { prisma };

fb34d4a75ecfa22c8407b2accfe7da501dfbff44
cylim <cylim226@gmail.com>
2024-01-22 16:36:28 +0800

    style(email): add space before verify code

--- packages/notifications/src/templates/otp-verification.tsx
+++ packages/notifications/src/templates/otp-verification.tsx
@@ -5,7 +5,7 @@ export const OTPTemplate = ({ code }: { code: number }) => {
     <div>
       <p>Hello,</p>
       <p>
-        Your one-time password for verifying your email address is
+        Your one-time password for verifying your email address is&nbsp;
         <strong>{code}</strong>.&nbsp;
       </p>
     </div>
